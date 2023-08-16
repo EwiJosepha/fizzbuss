afcef514ee09b8e02f797284bc869cdef74c59cc
ewi josepha <ewijosepha@gmail.com>
2023-08-16 20:46:39 +0200

    fizzbuzz complete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= 100; i++){
     if (i % 3 == 0 && i % 5 == 0) console.log("FizzBuzz \n");
     else if (i % 3 == 0) console.log("Fizz \n");
     else if (i % 5 == 0) console.log("Buzz \n");
